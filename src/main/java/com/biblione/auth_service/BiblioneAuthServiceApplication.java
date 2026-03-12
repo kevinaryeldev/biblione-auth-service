7a3b2076ffad71432a8fd51255e8ad692361d717
Kevin Aryel <kevinaryel.dev@gmail.com>
2026-03-12 17:36:11 -0300

    chore: inicializaçÃca do projeto spring boot para o serviÃco de autenticacao

---
+++ src/main/java/com/biblione/auth_service/BiblioneAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.biblione.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioneAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioneAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblione/auth_service/BiblioneAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblione.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioneAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
